9ffe6dd10cbb20b6a40e833738d135ede9f5e87b
PTAE43 <kritsada1696@gmail.com>
2025-09-15 10:32:20 +0700

    Done: question-3

---
+++ question-3.js
@@ -0,0 +1,22 @@
+/*
+What is the Big O notation for Function findStudentById . Explain your reason.
+Answer: O(n) check them one by one. เป็นการค้นหาทีละตัว.
+
+
+*/
+
+/* 
+What is the Big O notation for Function findProductPrice. Explain your reason.
+Answer: O(log n) Because each time we cut off half of the data. เป็นการค้นหาแบบทีละครึ่ง.
+
+
+*/
+
+/* 
+Which function is more efficient and why?
+Answer: O(log n) It is faster than O(n) because it is a half search, 
+or cutting out data without searching one element at a time.
+ทำงานเร็วกว่า O(n) เพราะเป็นการค้นหาทีละครึ่ง หรือตัดข้อมูลออกโดยไม่ค้นหาทีละตัว.
+
+
+*/
